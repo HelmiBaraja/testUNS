79bc1dd16f9219337e8da1d4edf29ae481b86520
Helmi Hasan <helmi@terato.com>
2016-10-19 10:03:49 +0700

    add title and edition in Book class

--- testProject/app/src/main/java/com/pangestu/testproject/Book.java
+++ testProject/app/src/main/java/com/pangestu/testproject/Book.java
@@ -4,4 +4,18 @@ package com.pangestu.testproject;
  * Created by ubuntu on 10/19/16.
  */
 public class Book {
+
+    String title;
+    String edition;
+
+    public Book ()
+    {
+
+    }
+
+    public Book (String title, String edition)
+    {
+        this.title = title;
+        this.edition = edition;
+    }
 }
